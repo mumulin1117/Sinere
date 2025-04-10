3a20188e796f3e138cc9432b2581451e7f74e9ea
mumulin1117 <mumulin1117@163.com>
2025-04-10 17:48:12 +0800

    1.7.1

--- Sinere/InspireStation/SneTools.swift
+++ Sinere/InspireStation/SneTools.swift
@@ -55,7 +55,7 @@ class SneTools {
         return sneSafeBottom + 49.0
     }
     
-    static let sneImpact:Double = 1744246800//2025-04-10 09:00:00
+    static let sneImpact:Double = 1744422381//2025-04-12 09:46:21
     
     static let shareInfo = SneTools()
     
